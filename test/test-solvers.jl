2e6304300107c77db296c595c9099b92fd68ad74
Dominique <dominique.orban@gmail.com>
2026-04-29 11:42:55 -0400

    Unit tests for individual subsolvers

--- test/test-solvers.jl
+++ test/test-solvers.jl
@@ -1,6 +1,36 @@
 # using NLPModelsTest
 # using Test
 
+@testitem "single subproblem solve with Trunk" tags=[:solver, :unconstrained, :trunk] begin
+  using ADNLPModels, NLPModels, ProximalOperators
+  model = ADNLPModel(x -> (x[1] - 1.0)^2 + 100 * (x[2] - x[1]^2)^2, [-1.2; 1.0])
+  h = NormL1(1.0)
+  env_model = EnveloptNLPModel(model, h)  # F(x) = x and μ = 1 by default
+  trunk_solver = TrunkEnveloptSubSolver(env_model)
+  stats = trunk_solver(env_model, get_x0(env_model), tol = 1.0e-2)
+  @test Envelopt.first_order(stats)
+end
+
+@testitem "single subproblem solve with TRON" tags=[:solver, :unconstrained, :tron] begin
+  using ADNLPModels, NLPModels, ProximalOperators
+  model = ADNLPModel(x -> (x[1] - 1.0)^2 + 100 * (x[2] - x[1]^2)^2, [-1.2; 1.0])
+  h = NormL1(1.0)
+  env_model = EnveloptNLPModel(model, h)  # F(x) = x and μ = 1 by default
+  tron_solver = TronEnveloptSubSolver(env_model)
+  stats = tron_solver(env_model, get_x0(env_model), tol = 1.0e-2)
+  @test Envelopt.first_order(stats)
+end
+
+@testitem "single subproblem solve with MadNLP" tags=[:solver, :unconstrained, :madnlp] begin
+  using ADNLPModels, NLPModels, ProximalOperators
+  model = ADNLPModel(x -> (x[1] - 1.0)^2 + 100 * (x[2] - x[1]^2)^2, [-1.2; 1.0])
+  h = NormL1(1.0)
+  env_model = EnveloptNLPModel(model, h)  # F(x) = x and μ = 1 by default
+  madnlp_solver = MadNLPEnveloptSubSolver(env_model)
+  stats = madnlp_solver(env_model, get_x0(env_model), 0, tol = 1.0e-2)
+  @test Envelopt.first_order(stats)
+end
+
 @testitem "simple solve with MadNLP" tags=[:solver, :unconstrained, :madnlp] begin
   using ADNLPModels, MadNLP, ProximalOperators
   model = ADNLPModel(x -> (x[1] - 1.0)^2 + 100 * (x[2] - x[1]^2)^2, [-1.2; 1.0])
