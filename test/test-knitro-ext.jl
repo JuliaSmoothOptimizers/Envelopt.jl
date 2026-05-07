e30043b84076c457f4d312fc4d7622134416df5e
Dominique <dominique.orban@gmail.com>
2026-05-07 19:20:30 -0400

    Print less stuff in Knitro tests

--- test/test-knitro-ext.jl
+++ test/test-knitro-ext.jl
@@ -1,17 +1,19 @@
 @testmodule KnitroHelper begin
   using KNITRO
 
-  knitro_available = begin
-    try
-      # KNITRO.has_knitro() returns true even if there is no valid license, and seems fairly useless
-      KNITRO.KN_free(KNITRO.KN_new())
-      true
-    catch
-      false
-    end
+  knitro_available = try
+    # KNITRO.has_knitro() returns true even if there is no valid license, and seems fairly useless
+    KNITRO.KN_free(KNITRO.KN_new())
+    true
+  catch
+    false
   end
 
-  println("KNITRO available: ", knitro_available)
+  if knitro_available
+    println("KNITRO is available. Running Knitro tests.")
+  else
+    println("KNITRO is not available. Skipping Knitro tests.")
+  end
 end
 
 @testitem "Knitro not available by default" tags=[:solver, :knitro] begin
@@ -31,8 +33,6 @@ end
     knitro_solver = KnitroEnveloptSubSolver(env_model)
     stats = knitro_solver(env_model, get_x0(env_model), 0, tol = 1.0e-2)
     @test Envelopt.first_order(stats)
-  else
-    println("KNITRO is not available. Skipping Knitro test.")
   end
 end
 
@@ -49,8 +49,6 @@ end
     stats, status, u =
       envelopt(env_model; subsolver = KnitroEnveloptSubSolver(env_model), verbose = false)
     @test Envelopt.first_order(stats)
-  else
-    println("KNITRO is not available. Skipping Knitro test.")
   end
 end
 
@@ -68,8 +66,6 @@ end
     stats, status, u =
       envelopt(env_model; subsolver = KnitroEnveloptSubSolver(env_model), verbose = false)
     @test Envelopt.first_order(stats)
-  else
-    println("KNITRO is not available. Skipping Knitro test.")
   end
 end
 
@@ -87,7 +83,5 @@ end
     stats, status, u =
       envelopt(env_model; subsolver = KnitroEnveloptSubSolver(env_model), verbose = false)
     @test Envelopt.first_order(stats)
-  else
-    println("KNITRO is not available. Skipping Knitro test.")
   end
 end
